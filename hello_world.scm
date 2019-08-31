5d6636425f562f713a98392c2cf0a2d73d0163d4
Prabu <meetprabu88@gmail.com>
2019-08-31 20:55:21 +0530

    Square root of a number using multiple functions

---
+++ hello_world.scm
@@ -0,0 +1,23 @@
+(define (new-if predicate then-clause else-clause)
+    (cond   (predicate then-clause) 
+            (else else-clause)))
+
+(define (average x y) (/ (+ x y) 2))
+
+(define (square x) (* x x))
+
+(define (improve guess x) 
+    (average guess (/ x guess)))
+
+(define (goodEnough? guess x) 
+    (< 
+        (abs (- (square guess) x))
+        0.000001))
+
+(define (sqrt-iter guess x) 
+    (new-if (goodEnough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+        
+(define (sqrt x) 
+    (sqrt-iter 1.0 x))
