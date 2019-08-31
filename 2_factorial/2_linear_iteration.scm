eb044feed5a43f726a90df46142bd43f0bc05e5c
Prabu <meetprabu88@gmail.com>
2019-08-31 22:02:06 +0530

    Recursive process and procedure

--- 2_factorial/2_linear_iteration.scm
+++ 2_factorial/2_linear_iteration.scm
@@ -1,3 +1,7 @@
+; Recursive Process vs Recursive procedure
+; Recursive procedure - it is because the procedure is calling itself
+; Recursive Process - it is because of the way the process evolves (non tail-recursive process)
+
 (define (factorial max-count)
     (define (factorial-iter value current-index)
     (if (> current-index max-count)
